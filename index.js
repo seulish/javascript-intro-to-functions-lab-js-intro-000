ab6a810e9b4d5a618ffb3414e2294d779dafd2a6
Leah Lee <leah@iterable.com>
2021-02-04 22:27:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string === string.toLowerCase()
+  if string.toLowerCase() === string
     return "I cannot hear you!"
-  else if string === string.toUpperCase()
+  else if string.toUpperCase() === string
     return "YES INDEED!"
-  else
+  else if string === "I love you, Grandma"
     return "I love you, too."
 }
